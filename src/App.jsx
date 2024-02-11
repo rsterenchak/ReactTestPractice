11a7ad72bfb8ae919b9cc716f996a57503e737d5
rsterenchak <rsterenchak@gmail.com>
2024-02-10 23:27:29 -0800

    made test changes, testing suite

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+/* function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -30,6 +30,8 @@ function App() {
       </p>
     </>
   )
-}
+} */
+
+const App = () => <h1>Our First Test</h1>;
 
 export default App
